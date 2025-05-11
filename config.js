a1d518fd74da053136206705572e142f00fbe06f
Abrar966 <carl81726483@gmail.com>
2025-05-11 09:24:49 +0500

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ const settingsMenu = [
   ]
 DATABASE_URL = process.env.DATABASE_URL === undefined ? './bot.db' : process.env.DATABASE_URL;
 DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG);
-if (!(process.env.SESSION || process.env.SESSION_ID)) throw new Error("Raganork~45675fc52a196209f17e1eb97445c6fa:674c5a4c64374c485a55633d")
+if (!(process.env.SESSION || process.env.Raganork~45675fc52a196209f17e1eb97445c6fa:674c5a4c64374c485a55633d)) throw new Error("Raganork~45675fc52a196209f17e1eb97445c6fa:674c5a4c64374c485a55633d")
 module.exports = {
     VERSION: 'v4.0.0',
     ALIVE: process.env.ALIVE || "https://i.imgur.com/KCnoMM2.jpg Hey {sender}, I'm alive \n Uptime: {uptime}",
